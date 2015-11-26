3dbe07681bf234a733c4ff7fbf4fe7767cd9accd
Patrick Mazulo <pmazulo@gmail.com>
2015-11-26 00:06:39 -0300

    Create more tests on function_tests.py

--- tdd/superlists/functional_tests.py
+++ tdd/superlists/functional_tests.py
@@ -44,12 +44,24 @@ class NewVisitorTest(unittest.TestCase):
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
+        self.fail('Finish the test!')
+
         # She visits that URL - her to-do list is still there.
         # Satisfied, she goes back to sleep
 if __name__ == '__main__':
