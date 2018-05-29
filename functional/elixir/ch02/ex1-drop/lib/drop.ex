7a7fac4d052c26d0d475cfc4c173dd011121a3e1
Patrick Mazulo <pmazulo@gmail.com>
2018-05-28 23:06:46 -0300

    Add doc to the drop module

--- functional/elixir/ch02/ex1-drop/lib/drop.ex
+++ functional/elixir/ch02/ex1-drop/lib/drop.ex
@@ -1,8 +1,14 @@
 defmodule Drop do
-  # From :math module, only get sqrt function, with arity 1
+  @doc"""
+  Calculates the velocity of an object falling on Earth
+  as if it were in a vacuum (no air resistance). The distance is
+  the height from which the object falls, specified in meters,
+  and the functions returns a velocity in meters per second.
+  """
   import :math, only: [sqrt: 1]
-  def fall_velocity(distance) do
-    sqrt(2 * 9.8 * distance)
+  # From :math module, only get sqrt function, with arity 1
+  def fall_velocity(distance, gravity \\ 9.8) do
+    sqrt(2 * gravity * distance)
   end
 end
 
