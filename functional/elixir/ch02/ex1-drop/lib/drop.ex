4cf3a34758431cc10e0568bc863e1bf741fa760a
Patrick Mazulo <pmazulo@gmail.com>
2018-05-28 22:45:12 -0300

    Move Convert defmodule to its own file

--- functional/elixir/ch02/ex1-drop/lib/drop.ex
+++ functional/elixir/ch02/ex1-drop/lib/drop.ex
@@ -1,16 +1,9 @@
 defmodule Drop do
+  # From :math module, only get sqrt function, with arity 1
+  import :math, only: [sqrt: 1]
   def fall_velocity(distance) do
-    :math.sqrt(2 * 9.8 * distance)
+    sqrt(2 * 9.8 * distance)
   end
 end
 
-defmodule Convert do
-  def mps_to_mph(mps) do
-    2.23693629 * mps
-  end
-
-  def mps_to_kph(mps) do
-    3.6 * mps
-  end
-end
 
